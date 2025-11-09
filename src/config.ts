0975fe1882fa1f24c8095215fd59fed53d09e91b
unknown <frhanhanif@gmail.com>
2025-11-09 11:36:35 +0700

    update 4.1

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Farhan Hanifputra';
-export const SITE_DESCRIPTION = 'This Farhan Hanifputra portfolio website built with Astro.';
+export const SITE_DESCRIPTION = 'This Farhan Hanifputra portfolio website.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = false
